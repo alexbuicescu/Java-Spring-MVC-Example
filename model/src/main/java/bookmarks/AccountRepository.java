103eebe8b9db9505ed83b75e4a91f1fd0f209f8f
alexbuicescu <alexbuicescu@gmail.com>
2015-10-17 03:36:35 +0300

    first commit

---
+++ model/src/main/java/bookmarks/AccountRepository.java
@@ -0,0 +1,10 @@
+package bookmarks;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByUsername(String username);
+}
